36c48f7044eaf76f8b61f959226bbbb177d9198d
YO1-SAI10 <flower.blooms.in.the.heart@gmail.com>
2025-11-19 08:38:08 +0900

    Update Program.cs
    2か所変更

--- Program.cs
+++ Program.cs
@@ -10,12 +10,14 @@ namespace Problem7_3
     {
         static void Main(string[] args)
         {
+        	//プログラム更新足跡
             Calculation c = new Calculation();
             c.Number1 = 8;    //  一つ目の数をセット
             c.Number2 = 9;    //  二つ目の数をセット
             //  二つの数の和を表示
             Console.WriteLine("{0} + {1} = {2}", c.Number1, c.Number2, c.Add());
             //  二つの数の差を表示
+            //AAA
             Console.WriteLine("{0} - {1} = {2}", c.Number1, c.Number2, c.Sub());
         }
     }
