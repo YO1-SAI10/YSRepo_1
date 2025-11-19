7db223ef5da775340dfcae6987e5aaea0383a487
YO1-SAI10 <flower.blooms.in.the.heart@gmail.com>
2025-11-19 09:35:21 +0900

    Update Program.cs
    １か所追加、１か所変更

--- Program.cs
+++ Program.cs
@@ -15,8 +15,9 @@ namespace Problem7_3
             c.Number1 = 8;    //  一つ目の数をセット
             c.Number2 = 9;    //  二つ目の数をセット
             //  二つの数の和を表示
+            2回目の変更
             Console.WriteLine("{0} + {1} = {2}", c.Number1, c.Number2, c.Add());
-            //  二つの数の差を表示
+            //  二つの数の差を表示する
             //AAA
             Console.WriteLine("{0} - {1} = {2}", c.Number1, c.Number2, c.Sub());
         }
